893bf6c54ab3b4d75bbeadc0abef1bcff4419ecd
Svanhvit <svanhviti16@ru.is>
2018-03-20 01:08:46 +0000

    cartoon network spinner

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Col from "./components/Col/Col";
 import Carousel from "./components/Carousel/Carousel";
 import Tabs from "./components/Tabs/Tabs";
 import Tab from "./components/Tab/Tab"
+import CartoonNetworkSpinner from "./components/CartoonNetworkSpinner/CartoonNetworkSpinner";
 
 class App extends React.Component {
   constructor(props) {
@@ -43,7 +44,17 @@ class App extends React.Component {
           <Carousel
             images={[
               'https://cdn.dribbble.com/users/1808107/screenshots/4121267/04_finn.gif',
-              'https://t1.rbxcdn.com/12859a4e26433b868b22ab48ac1ce5b2'
+              'https://t1.rbxcdn.com/12859a4e26433b868b22ab48ac1ce5b2',
+              'https://vignette.wikia.nocookie.net/cartoon-network-characters/images/6/6a/Skips_character.png/revision/latest?cb=20130310180547',
+              'http://moziru.com/images/cartoon-network-clipart-4.png',
+              'http://big5kayakchallenge.com/wp-content/uploads/ktz/cool-cartoon-network-characters-wiki-image-bubbles-2016-design-the-cartoon-network-wiki-cartoon-network-characters-wiki-35bunbhbzb2sw4jz2jlpmy.png',
+              'http://images6.fanpop.com/image/photos/32600000/Vana-cartoon-networks-sidekick-32646899-160-300.png',
+              'http://i.imgur.com/Xx1ORD5.png',
+              'https://vignette.wikia.nocookie.net/parody/images/e/ed/Gumball_watterson_cartoon_network.png/revision/latest?cb=20150808071702',
+              'https://vignette.wikia.nocookie.net/powerpuff/images/c/ca/Stylowi_pl_hobby_bojka--postacie-z-atomowki--cartoon-network_8140914.png/revision/latest?cb=20150724164644',
+              'https://img.buzzfeed.com/buzzfeed-static/static/2015-04/7/18/enhanced/webdr05/enhanced-7218-1428446543-1.png',
+              'http://i.imgur.com/4EViWPk.png',
+              'http://www.indiewire.com/wp-content/uploads/2013/10/adventure-time.png'
             ]}
             size="medium"/>
         <div className={`${styles.rowContainer}`}>
@@ -107,6 +118,7 @@ class App extends React.Component {
               Content 3
             </Tab>
           </Tabs>
+          <CartoonNetworkSpinner interval={4}/>
         </div>
       </div>
     );
